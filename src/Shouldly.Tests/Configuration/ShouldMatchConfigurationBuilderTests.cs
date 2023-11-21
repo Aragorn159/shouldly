b753fa17457be506aa13f7dec6b8a36bcb4cacc0
Michal Cocuľa <michal.cocula@student.tuke.sk>
2023-11-21 15:33:19 +0100

    Adding ShouldMatchConfigurationBuilderTests.

---
+++ src/Shouldly.Tests/Configuration/ShouldMatchConfigurationBuilderTests.cs
@@ -0,0 +1,21 @@
+﻿using Shouldly.Configuration;
+using Moq;
+
+namespace Shouldly.Tests.Configuration;
+
+public class ShouldMatchConfigurationBuilderTests
+{
+    [Fact]
+    public void DoNotIgnoreLineEndings_WhenIgnoreLineEndingsIsSet_ShouldRemoveIgnoreLineEndingsFlag()
+    {
+        // Arrange
+        var config = new ShouldMatchConfiguration();
+        config.StringCompareOptions = StringCompareShould.IgnoreLineEndings;
+        var builder = new ShouldMatchConfigurationBuilder(config);
+
+        builder.DoNotIgnoreLineEndings();
+
+        // Assert
+        Assert.Equal(StringCompareShould.IgnoreLineEndings, config.StringCompareOptions);
+    }
+}
